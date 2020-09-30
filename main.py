2a61715d58dce3d72a4ad50c7cc369f4b66ca114
윤연경 <amyss0907@gmail.com>
2020-09-30 11:53:00 +0900

    enfa2dfa

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from queue import Queue  
+
+
 class Delta : 
     def __init__(self, _start_state, _input, _end_state) :
         self.start_state = _start_state # list of integer
@@ -6,7 +9,7 @@ class Delta :
     
     def delta2str(self) :
         #returns a readable delta
-        return "delta{{" + str(self.start_state) + "}, " + str(self.input) + "} = {" + str(self.end_state) + "}" 
+        return "delta{" + str(self.start_state) + ", " + str(self.input) + "} = " + str(self.end_state)  
 
     def add_start(self, s) :
         self.start_state = self.start_state.append(s)
@@ -17,11 +20,38 @@ class Delta :
 class DFA:
 
     def __init__(self, _Q, _I, _D, _S, _F):
-        self.Q = _Q #
-        self.I = _I # 
-        self.D = _D # 
-        self.S = _S #
-        self.F = _F # 
+        self.Q = _Q #states
+        self.I = _I #input
+        self.D = _D #deltas
+        self.S = _S #start_states
+        self.F = _F #end_states 
+        self.closure = [] #cl for each state
+        self.cnt = 0
+
+    def cl_loop(self,s,i) :
+        cl = []
+        for k in s :
+            temp = str(k).replace("[", "")
+            temp = temp.replace("]","")
+            for d in self.D :
+                print("s and d",s,d.delta2str())
+                if temp in str(d.start_state) and  ( str(i) in str(d.input) or (self.cnt!=0 and 'e' in str(d.input)) ) :
+                    cl = cl + d.end_state
+                    self.cnt = self.cnt + 1
+                    cl = cl + self.cl_loop(d.end_state, 'e')
+                    self.cnt = self.cnt - 1
+        #print("cnt",self.cnt)
+        return cl
+
+    def cl (self,s,i) : #closure for each state
+        #for s in self.Q :
+        cl = []
+        cl = self.cl_loop(s,i)
+        #cl = cl + s
+        #print('cl',cl)
+        cl=list(set(cl))
+        return cl
+
 
     def deltas(self) :
         return self.D
@@ -48,7 +78,7 @@ class DFA:
         #print ([ i.delta2str() for i in self.D ], sep = "\n")
         #print( self.Q, self.I, self.S, self.F)
 
-from queue import Queue  
+
 
 def nfa2dfa (nfa) :
     delta=[] # list for deltas
@@ -66,11 +96,13 @@ def nfa2dfa (nfa) :
         for i in nfa.inputs() : # iterate over all the inputs
             #print("looking for",s,i)
             o = look_all_possible_next_states(nfa, s, i)
-            new_delta = Delta(s,i,o)
-            delta.append(new_delta)
-            if not any(o == i for i in states ) :
-                states.append(o)
-                q.put(o)
+            if o : #some states don't have next states at all
+                #print("oos",o)
+                new_delta = Delta(s,i,o)
+                delta.append(new_delta)
+                if not any(o == i for i in states ) :
+                    states.append(o)
+                    q.put(o)
         q.get() #remove from queue after searching once
 
     #print("num of states",len(states))
@@ -90,10 +122,12 @@ def determine_the_end_state(nfa,states) :
         temp = temp.replace("]","")
         if( temp in str(s))  :
             end.append(s)
+            #end.append(nfa.cl(s)) #for epsilon
     return end
 
+    
             
-def look_all_possible_next_states(nfa,s,i) :
+def look_all_possible_next_states(nfa,s,i) : # for each state
     new=[]
     #print(str(s))
     for n in nfa.deltas() :
@@ -102,7 +136,10 @@ def look_all_possible_next_states(nfa,s,i) :
         temp = temp.replace("]","")
         if (temp in str([s]) and n.input == i) :
             new = new + n.end_state
-    new=list(set(new))#get rid of dups        
+    new = new + nfa.cl(s, i) #for epsilon states
+    #print("before",nfa.cl(s))
+    new=list(set(new))#get rid of dups     
+    #print('s i ',s,i,new)   
     return new
 
 """
@@ -117,8 +154,11 @@ d.append(Delta([0],'a',[1]))
 d.append(Delta([0],'b',[0]))
 d.append(Delta([1],'b',[2]))
 d.append(Delta([2],'b',[3]))
-nfa1 = DFA(Q ,I, d, S, F)"""
+nfa1 = DFA(Q ,I, d, S, F)
+"""
 
+
+"""
 q = [(0),(1),(2),(3)]
 i = ['a','b']
 s=[(0)]
@@ -133,9 +173,25 @@ d.append(Delta([2],'b',[3]))
 d.append(Delta([3],'a',[3]))
 d.append(Delta([3],'b',[3]))
 nfa1=DFA(q,i,d,s,f)
+"""
 
 
-
+q = [('A'),('B'),('C'),('D'),('E')]
+i = [0,1]
+s=[('A')]
+f=[('D')]
+d=[]
+d.append(Delta(['A'],0,['E']))
+d.append(Delta(['A'],1,['B']))
+d.append(Delta(['B'],1,['C']))
+d.append(Delta(['B'],'e',['D']))
+d.append(Delta(['C'],1,['D']))
+d.append(Delta(['E'],0,['F']))
+d.append(Delta(['E'],'e',['B']))
+d.append(Delta(['E'],'e',['C']))
+d.append(Delta(['F'],0,['D']))
+#d.append(Delta(['A'],0,['E']))
+nfa1=DFA(q,i,d,s,f)
 
 print("NFA : ")
 nfa1.print()
